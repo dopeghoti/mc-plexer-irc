a6568fce85c9ae5db12423b28ee92e411725dfd3
Rob Palkowski <dopeghoti@gmail.com>
2011-01-04 11:35:19 -0700

    Gosh, too many changes to remember, but the darn thing works now.
    Note to self:  get a LOT better about commiting changes after the fact.

--- irc_class.py
+++ irc_class.py
@@ -17,7 +17,7 @@ class IRC:
 	events = []
 	socket = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 	print_next_event = True
-	outbox = ''
+	outbox = []
 
 	def __init__( self, server, port, nick, channel ):
 		#TODO: add error-checking and sanity checks
@@ -69,7 +69,7 @@ class IRC:
 		return( self.event_handler( next_ev ) )
 
 	def event_handler( self, event ):
-		self.outbox = ''
+		temp_outbox = ''
 		self.print_next_event = True
 		self.data = event.rstrip( '\r' )
 		sdata = self.data.split( ' ' )
@@ -83,18 +83,55 @@ class IRC:
 			self.print_next_event = False
 		elif ( ( sdata[1] == 'PRIVMSG' ) and ( ( sdata[2] == self.channel ) or ( sdata[2] == self.nick ) ) ):
 			print( 'IRC > Message for me!')		#	XXX
-			#	The message is to us or the channel we're in.  Let's see if it's for Minecraft.
+			#	Raw IRC output:
+			#		0						1	2		3
+			#	IRC > :DopeGhoti!~ghoti@ip98-177-177-13.ph.ph.cox.net PRIVMSG ###linkulator :Ignore this message
+			#	IRC > :DopeGhoti!~ghoti@ip98-177-177-13.ph.ph.cox.net PRIVMSG ###linkulator :but this should go > MC
+
+			#	Figuring out the nick:
+			#	      ------------------sdata[0]--------------------- --[1]-- --sdata[2]--- ---[sdata[3]-------
+			#	      : Nick    !~user @ Host
+			#	IRC > :DopeGhoti!~ghoti@ip98-177-177-13.ph.ph.cox.net PRIVMSG ###linkulator :Ignore this message
+			#	therefore,
+			#	userhost = sdata[0]
+			#	nick = userhost.lstrip( ':' ).split( '!' )[0]
+
+			#	Per above, pick out the nick
+			userhost = sdata[0]
+			nick = userhost.lstrip( ':' ).split( '!' )[0]
+
+			#	At this point, we know the message is to us or the channel we're in.  Let's see if it's for Minecraft.
 			#	The message should end with "> MC".
 			if sdata[-2:] == ['>', 'MC']:
-				self.outbox = ' '.join( sdata[3:-2] )		#	If they just said "> MC", this will be a null string.
-				self.outbox = self.outbox.lstrip( ':' )		#	That pesky IRC protocol colon.
+				temp_outbox = ' '.join( sdata[3:-2] )		#	If they just said "> MC", this will be a null string.
+				temp_outbox = temp_outbox.lstrip( ':' )		#	That pesky IRC protocol colon.
+				temp_outbox = 'IRC: <' + nick + '> ' + temp_outbox
+				self.outbox.append( temp_outbox )
+
+			#	As suggested, an alternative way to send things to minecraft.
+			#	The message should start with "Botnick: "
+			if sdata[3] == ':' + self.nick + ':':
+				tmpdata = sdata[:]
+				if tmpdata[-2:] == ['>', 'MC']:
+					tmpdata = sdata[:-2]	#	They said Nick: foo > MC. Suppress the extra '> MC'
+				tmpdata = sdata[4:]
+				temp_outbox = 'IRC: <' + nick + '> ' 
+				temp_outbox += ' '.join( tmpdata )
+				self.outbox.append( temp_outbox )
+
+			if sdata[-2:] == ['>', 'IRC']:
+				self.say( 'Seriously?' )
+				temp_outbox = ' '.join( sdata[3:-2] )		#	If they just said "> MC", this will be a null string.
+				temp_outbox = temp_outbox.lstrip( ':' )		#	That pesky IRC protocol colon.
+				temp_outbox = 'IRC: <' + nick + '> ' + temp_outbox
+				self.outbox.append( temp_outbox )
+				
 			#	Now, handle some special commands:
+			#	TODO: check for leading '?', then check for length >= 2; command keywords
 			if sdata[3].lstrip( ':' ) in ('?server', '?minecraft' ):
 				self.say( 'The minecraft server can be found at ghoti.dyndns.org.  For more information, say "#link 673".' )
-
-		#		0						1	2		3
-		#	IRC > :DopeGhoti!~ghoti@ip98-177-177-13.ph.ph.cox.net PRIVMSG ###linkulator :Ignore this message
-		#	IRC > :DopeGhoti!~ghoti@ip98-177-177-13.ph.ph.cox.net PRIVMSG ###linkulator :but this should go > MC
+			if sdata[3].lstrip( ':' ) in ( '?who', '?players' ):
+				self.say( 'Not yet implemented.' )
 
 		return( self.data )
 
--- mc-irc.py
+++ mc-irc.py
@@ -7,66 +7,70 @@ import string
 import select
 
 import irc_class
+import mp_class
 
 # TODO: External configuration files
 
 # IRC configuration settings
 irc_server = 'irc.freenode.net'
-irc_nick = 'BlockHead'
+irc_nick = 'VoxelHead'
 irc_port = 6667
-irc_channel = '###linkulator'
+#irc_channel = '###linkulator'
+irc_channel = '##crawl-offtopic'
 
 # Multiplexer configuration settings
 mc_socket = '/home/minecraft/tmp/plexer.sock'
 mc_port = 9001
-mc_password = 'aardvark'
+mc_password = '[REDACTED]'
+
+sock_list = []
 
 # So, let's connect to IRC!
 print( 'Attempting to connect to IRC' )
 irc_conn = irc_class.IRC( irc_server, irc_port, irc_nick, irc_channel )
 
+if( irc_conn.status['connected'] ):
+	sock_list.append( irc_conn.socket )
+
 # And now, let's connect to the Minecraft Multiplexer
 # TODO: Exception handling for Plexer not being there to connect to
-# TODO: Make this a class like IRC.
 print( 'Attempting to connect to Minecraft Multiplexer' )
-ml = multiplexlib.MinecraftRemote(socket.AF_UNIX, mc_socket, mc_port, mc_password )
-ml.connect()
-print( 'Multiplexer connected.' )
+mc_conn = mp_class.multiplexer_connection( '/home/minecraft/tmp/plexer.sock', None, 'aardvark' )
+mc_conn.connect()
 
-# We don't want to look for args as this isn't an instant client. What we do want to do
-# is connect to IRC.
+if( mc_conn.status['connected'] ):
+	sock_list.append( mc_conn.socket )
 
-# Vestigal code from the generic plexer client.
-
-#if len(sys.argv) > 1:
-#    ml.send_command('%s' % string.join(sys.argv[1:], " ").decode('utf-8'))
-#    (sout, _, _) = select.select([ml.client_socket], [], [], 1)
-#
-#    if sout == []:
-#        exit()
-#    else:
-#        print ml.receive()
-#        ml.disconnect()
-#else:
+# sock_list = [ mc_conn.socket, irc_conn.socket ]
 
 try:
-	while irc_conn.status['connected']:
+	while irc_conn.status['connected'] and mc_conn.status['connected']:
+		( sock_out, sock_in, sock_exception ) = select.select( sock_list, [], [] )
+		# See if there is anything pending from Minecraft or IRC
+		for s in sock_out:
+			if s == mc_conn.socket:
+				mc_conn.cycle()
+			if s == irc_conn.socket:
+				irc_conn.cycle()
 
-	# See if there is anything pending _from_ IRC
-		irc_conn.cycle()
+		# Make sure the IRC connection hasn't died on us
+		if( irc_conn.status['connected'] == False ):
+			irc_conn.connect()
 
-	# see if there is anything pending _from_ Minecraft
-		pass
-		#mc.cycle
-
-	# See if there is anything pending _for_ IRC
+		# See if there is anything pending _for_ IRC
 		if irc_conn.status['joined'] == False:
 			irc_conn.join()
+		if( len( mc_conn.outbox ) > 0 ):
+			for message in mc_conn.outbox:
+				irc_conn.say( message )
+			mc_conn.outbox = []
 
-	# See if there is anything pending _for_ Minecraft
-		if irc_conn.outbox != '':
-			#there is!
-			print( 'MCM < ' + irc_conn.outbox + ' (pretend)' )
+		# See if there is anything pending _for_ Minecraft
+		if(len( irc_conn.outbox ) > 0):
+			for message in irc_conn.outbox:
+				print( 'MCM < ' + message )
+				mc_conn.cmd( 'say ' + message )
+			irc_conn.outbox = []
 
 #	More vestigal code from the generic client.
 
@@ -99,6 +103,6 @@ try:
 except KeyboardInterrupt:
     print 'Exiting.'
     irc_conn.disconnect( 'Process caught SIGINT' )
-except Exception, e:
-    print 'Got exception: ' + e.__str__()
+#except Exception, e:
+#    print 'Got exception: ' + e.__str__()
 
